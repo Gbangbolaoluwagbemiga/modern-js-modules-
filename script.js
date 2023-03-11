211268b67644e70b6040b1648a7a95df5f5a2945
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-11 21:42:32 +0100

    module continues

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Named export
-import { understandingModule } from './module.js';
-understandingModule();
-console.log(`i am a normal js`);
+// import { understandingModule } from './module.js';
+// understandingModule();
+// console.log(`i am a normal js`);
 
 // Default export
 import all from './module.js';
-all.understandingModule();
 console.log(`i am a normal js`);
+all();
